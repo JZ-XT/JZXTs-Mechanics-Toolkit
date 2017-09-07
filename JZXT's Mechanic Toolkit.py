a4e515c8a10f9e79fbbdcb4ef422d067b62325d8
Jay <31019085+JZ-XT@users.noreply.github.com>
2017-09-07 21:08:31 +1200

    V1.3
    Python 3 Support Added

--- JZXT's Mechanic Toolkit.py
+++ JZXT's Mechanic Toolkit.py
@@ -1,11 +1,11 @@
 def print_menu():
-    print 30 * "-" , "JZXT's Mechanic's Toolkit" , 30 * "-"
-    print "1. KW to HP Conversion"
-    print "2. HP to KW Conversion"
-    print "3. Feature Not Available"
-    print "4. Feature Not Available"
-    print "5. Exit"
-    print 87 * "-"
+    print (30 * "-" , "JZXT's Mechanic's Toolkit" , 30 * "-")
+    print ("1. KW to HP Conversion")
+    print ("2. HP to KW Conversion")
+    print ("3. Feature Not Available")
+    print ("4. Feature Not Available")
+    print ("5. Exit")
+    print (87 * "-")
 
   
 while True:
@@ -13,23 +13,23 @@ while True:
     choice = input("Enter your choice [1-5]: ")
      
     if choice==1:     
-        print "You Have Selected KW to HP Conversion"
+        print ("You Have Selected KW to HP Conversion")
         kw = input ('How many KW is the engine making? ')
         hp = kw / 0.745699872
         print(str(kw) + ' KW is equivalent to {0:.2f}'.format(hp) + ' HP')
-        print ""
+        print ("")
     elif choice==2:
-        print "You Have Selected HP to KW Conversion"
+        print ("You Have Selected HP to KW Conversion")
         hp = input ('How much HP is the engine making? ')
         kw = hp * 0.745699872
         print(str(hp) + ' HP is equivalent to {0:.2f}'.format(kw) + ' KW')
-        print ""
+        print ("")
     elif choice==3:
-        print "This feature is not yet available"
-        print ""
+        print ("This feature is not yet available")
+        print ("")
     elif choice==4:
-        print "This feature is not yet available"
-        print ""
+        print ("This feature is not yet available")
+        print ("")
     elif choice==5:
-        print "Exiting program...\n"
+        print ("Exiting program...\n")
         break
