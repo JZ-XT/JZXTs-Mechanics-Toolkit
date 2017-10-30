3fc19ffd68ec8a6d6080d45c4285eb6be4049c84
Jay <31019085+JZ-XT@users.noreply.github.com>
2017-10-31 10:59:19 +1300

    Update JZXT's Mechanic Toolkit.py

--- JZXT's Mechanic Toolkit.py
+++ JZXT's Mechanic Toolkit.py
@@ -13,18 +13,18 @@ while True:
     choice = input("Enter your choice [1-5]: ")
     if choice==1:     
         print ("You Have Selected KW to HP Conversion")
-        kw = input ('How many KW is the engine making? ')
+        kw = input ('How many KW is the car making? ')
         hp = kw / 0.745699872
         print(str(kw) + ' KW is equivalent to {0:.2f}'.format(hp) + ' HP\n') # \n == newline carrage, acheives the same as print('')
     elif choice==2:
         print ("You Have Selected HP to KW Conversion")
         hp = input ('How much HP is the engine making? ')
         kw = hp * 0.745699872
-        print(str(hp) + ' HP is equivalent to {0:.2f}'.format(kw) + ' KW\n')
+        print(str(hp) + ' HP is equivalent to {0:.2f}'.format(kw) + ' KW\n\n')
     elif choice==3:
-        print ("This feature is not yet available\n")
+        print ("This feature is not yet available\n\n")
     elif choice==4:
-        print ("This feature is not yet available\n")
+        print ("This feature is not yet available\n\n")
     elif choice==5:
         print ("Exiting program...\n")
         break
